4439cb52e4b048e187d04f3e101343a44e0e293b
lverghese <loganverghese@gmail.com>
2021-08-24 23:35:53 -0400

    manager tests setup

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,14 @@
+const { TestWatcher } = require('jest');
 const Manager = require('../lib/Manager');
+
+test('creates manager object', () => {
+    const manager = new Manager('Logan', 30, 'loganverghese@gmail.com', 7);
+
+    expect(manager.office).toEqual(expect.any(Number));
+});
+
+test('gets main role', () => {
+    const manager = new Manager('Logan', 30, 'loganverghese@gmail.com', 7);
+
+    expect(manager.getTheRole()).toEqual('Manager')
+})
