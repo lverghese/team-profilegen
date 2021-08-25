431e4bf7564bf89b54463eb5319d22aa664dea96
lverghese <loganverghese@gmail.com>
2021-08-24 23:21:43 -0400

    intern test setup

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,20 @@
 const Intern = require('../lib/Intern');
+
+//creates intern object
+test('creates intern object', () => {
+    const intern = new Intern('Logan', 30, 'loganverghese@gmail.com', 'UCF');
+
+    expect(intern.uni).toEqual(expect.any(String));
+});
+
+test('gets interns school', () => {
+    const intern = new Intern('Logan', 30, 'loganverghese@gmail.com', 'UCF')
+
+    expect(intern.getTheSchool()).toEqual(expect.stringContaining(engineer.uni.toString()));
+}); 
+
+test('gets main role', () => {
+    const intern = new Intern('Logan', 30, 'loganverghese@gmail.com', 'UCF');
+
+    expect(intern.getTheRole()).toEqual('Intern');
+})
